c092f3f55feddb23e8d449d446fb444f24f87d19
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2022-02-04 21:38:23 +0100

    add sources.sh script

---
+++ config/includes.chroot_before_packages/var/uflinuxofflinepackages/sources.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "
+deb http://deb.debian.org/debian/ bullseye main contrib non-free
+#deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
+
+deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+#deb-src http://security.debian.org/ bullseye-security main contrib non-free
+
+deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+#deb-src http://ftp.debian.org/debian/ bullseye-updates main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free
+#deb-src http://ftp.debian.org/debian/ bullseye-backports main contrib non-free
+" > /etc/apt/sources.list
+apt update
