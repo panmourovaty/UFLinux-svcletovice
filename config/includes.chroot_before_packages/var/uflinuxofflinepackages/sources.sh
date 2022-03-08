3a9cdd7ba42e58b9c79ac89c6176e921f2c6b62c
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2022-03-08 13:05:25 +0100

    remove adwaita set on first boot

--- config/includes.chroot_before_packages/var/uflinuxofflinepackages/sources.sh
+++ config/includes.chroot_before_packages/var/uflinuxofflinepackages/sources.sh
@@ -17,7 +17,6 @@ deb http://deb.debian.org/debian bullseye-backports main contrib non-free
 #deb-src http://ftp.debian.org/debian/ bullseye-backports main contrib non-free
 " > /etc/apt/sources.list
 apt-get autoremove uf-calamares-settings calamares -y
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
 rm -f /etc/apt/sources.list.d/uflinuxofflinepackages.list
 rm -f /etc/cron.d/uflinuxofflinepackages
 rm -rf /var/uflinuxofflinepackages
