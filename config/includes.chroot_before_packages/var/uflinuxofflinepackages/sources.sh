c2b3155a30f617e4d7b13479b08a92e8fe326572
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2022-03-08 11:11:41 +0100

    several fixes for d-i

--- config/includes.chroot_before_packages/var/uflinuxofflinepackages/sources.sh
+++ config/includes.chroot_before_packages/var/uflinuxofflinepackages/sources.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ $(hostname) -eq "uflinux-live" ]]
+then
+	echo "Live Mode detected, script wont be executed"
+else
 echo "
 deb http://deb.debian.org/debian/ bullseye main contrib non-free
 #deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
@@ -12,6 +16,9 @@ deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
 deb http://deb.debian.org/debian bullseye-backports main contrib non-free
 #deb-src http://ftp.debian.org/debian/ bullseye-backports main contrib non-free
 " > /etc/apt/sources.list
+apt-get autoremove uf-calamares-settings calamares -y
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
 rm -f /etc/apt/sources.list.d/uflinuxofflinepackages.list
 rm -f /etc/cron.d/uflinuxofflinepackages
 rm -rf /var/uflinuxofflinepackages
+fi
