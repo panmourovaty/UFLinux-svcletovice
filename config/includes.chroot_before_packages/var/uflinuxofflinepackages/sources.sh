ebe522a37da2e85271fa0887210db976a3995cd9
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2022-03-09 11:30:36 +0100

    ensure proper grub and keyboard config

--- config/includes.chroot_before_packages/var/uflinuxofflinepackages/sources.sh
+++ config/includes.chroot_before_packages/var/uflinuxofflinepackages/sources.sh
@@ -17,6 +17,25 @@ deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
 deb http://deb.debian.org/debian bullseye-backports main contrib non-free
 #deb-src http://ftp.debian.org/debian/ bullseye-backports main contrib non-free
 " > /etc/apt/sources.list
+echo 'GRUB_DEFAULT="saved"
+GRUB_DISABLE_RECOVERY="true"
+GRUB_DISABLE_SUBMENU="true"
+GRUB_HIDDEN_TIMEOUT="0"
+GRUB_HIDDEN_TIMEOUT_QUIET="true"
+GRUB_RECORDFAIL_TIMEOUT="$GRUB_TIMEOUT"
+GRUB_TERMINAL_OUTPUT="console"
+GRUB_TIMEOUT="0"
+GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
+GRUB_DISTRIBUTOR="UFlinux"' > /etc/default/grub
+plymouth-set-default-theme uf-plymouth-theme -R
+update-grub
+cat /etc/default/locale | grep 'KEYMAP='
+if [ $? -eq 1 ]
+then
+echo "KEYMAP=us" >> /etc/default/locale
+else
+fi
+sed 's/XKBMODEL=""/XKBMODEL="pc105"/g' /etc/default/keyboard
 apt-get autoremove uf-calamares-settings calamares -y
 flatpak update --assumeyes --noninteractive
 rm -f /etc/apt/sources.list.d/uflinuxofflinepackages.list
